a7ab8a97c6e0a9cabffd8f5ec80762a1f93618d8
xietian.faillone <xietian.faillone@bytedance.com>
2022-04-28 11:25:30 +0800

    fix: 修复 of 的bug

--- observables/of.ts
+++ observables/of.ts
@@ -2,16 +2,25 @@ import { fromArray } from './from';
 import { stringLike } from "../string/stringLike";
 
 export type of<
-    T1 extends stringLike,
-    T2 extends stringLike = '',
-    T3 extends stringLike = '',
-    T4 extends stringLike = '',
-    T5 extends stringLike = '',
-    T6 extends stringLike = '',
-    T7 extends stringLike = '',
-    T8 extends stringLike = '',
-    T9 extends stringLike = '',
-    T10 extends stringLike = '',
-    T11 extends stringLike = '',
-    T12 extends stringLike = ''
-> = fromArray<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12]>
+    T1 extends unknown,
+    T2 extends unknown,
+    T3 extends unknown = unknown,
+    T4 extends unknown = unknown,
+    T5 extends unknown = unknown,
+    T6 extends unknown = unknown,
+    T7 extends unknown = unknown,
+    T8 extends unknown = unknown,
+    T9 extends unknown = unknown,
+    T10 extends unknown = unknown,
+    T11 extends unknown = unknown,
+    T12 extends unknown = unknown
+> = fromArray<
+    removeunknown<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12]>
+>
+
+type removeunknown<T extends unknown[]> =
+    T extends [infer Item, ...infer Rest]
+            ? Item extends stringLike
+                ? [Item, ...removeunknown<Rest>]
+                : removeunknown<Rest>
+            : []
